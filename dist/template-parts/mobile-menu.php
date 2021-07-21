647a1ce1ac2197c76b837263711475c3a9bcf9f8
MediaBay <med.bay@yandex.ru>
2021-07-21 18:30:50 +0300

    mobile menu

--- dist/template-parts/mobile-menu.php
+++ dist/template-parts/mobile-menu.php
@@ -1,13 +1,15 @@
 <aside class="menu" style="display:none">
-  <div class="menu__cnt">
+  <div class="menu__cnt container lazy" data-src="#">
     <div class="menu__hdr">
-      <button type="button" class="menu__close"></button>
       <a href="<?php echo $site_url ?>" class="menu__logo">
         <img src="<?php echo $logo_url ?>" alt="Логотип Manifest Magazine" class="menu__logo-img">
       </a>
+      <button type="button" class="menu__close">
+        <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 20" class="menu__close-svg"><path stroke="currentColor" d="M20.4455.353553L1.35359 19.4454M19.7384 19.4455L.646481.353591" class="menu__close-path"/></svg>
+      </button>
     </div> <?php
     wp_nav_menu( [
-      'theme_location'  => 'header_menu',
+      'theme_location'  => 'mobile_menu',
       'container'       => 'nav',
       'container_class' => 'menu__nav',
       'menu_class'      => 'menu__nav-list',
